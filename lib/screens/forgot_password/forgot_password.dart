fea40c604bb353ebd28b307038ba70945fb90c34
ahmadexe <muahmad710@gmail.com>
2024-01-27 02:11:01 +0500

    feat(ui): forgor pass screen done

--- lib/screens/forgot_password/forgot_password.dart
+++ lib/screens/forgot_password/forgot_password.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:form_builder_validators/form_builder_validators.dart';
+import 'package:lottie/lottie.dart';
 
 import 'package:prism/configs/configs.dart';
 import 'package:flutter/material.dart';
 import 'package:prism/static/app_statics.dart';
+import 'package:prism/widgets/design/buttons/app_button.dart';
+import 'package:prism/widgets/design/input/app_text_field.dart';
 import 'package:provider/provider.dart';
 
 import 'package:flutter_form_builder/flutter_form_builder.dart';
--- lib/screens/forgot_password/widgets/_body.dart
+++ lib/screens/forgot_password/widgets/_body.dart
@@ -9,6 +9,7 @@ class _Body extends StatelessWidget {
 
     return SafeArea(
       child: Scaffold(
+        resizeToAvoidBottomInset: false,
         appBar: AppBar(
           title: const Text('Forgot Password'),
           actions: [
@@ -19,8 +20,56 @@ class _Body extends StatelessWidget {
             ),
           ],
         ),
-        body: Column(
-          children: [],
+        body: Padding(
+          padding: Space.all(),
+          child: FormBuilder(
+            key: screenState.formKey,
+            initialValue: _FormData.initialValues(),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Space.y2!,
+                Center(
+                  child: Lottie.asset(
+                    AppStaticData.forgotPassAnimation,
+                    height: AppDimensions.normalize(140),
+                    repeat: true,
+                    reverse: true,
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                Space.yf(65),
+                Text(
+                  'Forgot Password?',
+                  style: AppText.h1b,
+                ),
+                Space.y!,
+                Text(
+                  "Don't worry, we got you covered!",
+                  style: AppText.b1,
+                ),
+                Space.y2!,
+                AppTextField(
+                  name: _FormKeys.email,
+                  hint: 'Enter email address',
+                  textCapitalization: TextCapitalization.none,
+                  prefixIcon: const Icon(Icons.mail),
+                  validator: FormBuilderValidators.compose(
+                    [
+                      FormBuilderValidators.required(),
+                      FormBuilderValidators.email(),
+                    ],
+                  ),
+                ),
+                Space.y2!,
+                AppButton(
+                  onPressed: () {},
+                  label: 'Send Email',
+                  buttonType: ButtonType.secondary,
+                ),
+              ],
+            ),
+          ),
         ),
       ),
     );
