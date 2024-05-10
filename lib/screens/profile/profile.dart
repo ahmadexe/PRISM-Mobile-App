3724591c9647c129bbbc7cd1b45e4e2b7d541243
ahmadexe <muahmad710@gmail.com>
2024-05-10 15:21:16 +0500

    feat: users post displayed

--- lib/screens/profile/profile.dart
+++ lib/screens/profile/profile.dart
@@ -3,10 +3,13 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:iconsax/iconsax.dart';
 import 'package:intl/intl.dart';
 import 'package:prism/blocs/auth/bloc.dart';
+import 'package:prism/blocs/posts/bloc.dart';
 import 'package:prism/configs/configs.dart';
 import 'package:flutter/material.dart';
+import 'package:prism/models/post/post.dart';
 import 'package:prism/router/routes.dart';
 import 'package:prism/static/app_statics.dart';
+import 'package:prism/widgets/avatar.dart';
 import 'package:prism/widgets/core/bottom_bar/bottom_bar.dart';
 import 'package:prism/widgets/info_tile.dart';
 import 'package:provider/provider.dart';
@@ -15,6 +18,7 @@ part '_state.dart';
 
 part 'widgets/_body.dart';
 part 'widgets/_tab.dart';
+part 'widgets/_post_meta_card.dart';
 
 class ProfileScreen extends StatelessWidget {
   const ProfileScreen({Key? key}) : super(key: key);
---
+++ lib/screens/profile/widgets/_post_meta_card.dart
@@ -0,0 +1,46 @@
+part of '../profile.dart';
+
+class _PostMetaCard extends StatelessWidget {
+  final Post post;
+  const _PostMetaCard({super.key, required this.post});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Row(
+          children: [
+            Expanded(
+              child: Row(
+                children: [
+                  Avatar(
+                    imageUrl: post.userProfilePic,
+                  ),
+                  Space.x2!,
+                  Text(
+                    post.userName,
+                    style: AppText.b1bm,
+                  ),
+                ],
+              ),
+            ),
+            Space.x!,
+            GestureDetector(
+              onTap: () {},
+              child: const Icon(Icons.more_vert),
+            )
+          ],
+        ),
+        Space.y1!,
+        Text(
+          post.title,
+          style: AppText.b1b,
+        ),
+        const Divider(
+          thickness: 0.5,
+        )
+      ],
+    );
+  }
+}
