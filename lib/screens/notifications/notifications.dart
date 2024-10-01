5aaf4e87f2a8885349d4fb74e720b46aab60dbf2
ahmadexe <muahmad710@gmail.com>
2024-10-01 19:38:18 +0500

    chore: ui clean up

--- lib/screens/notifications/notifications.dart
+++ lib/screens/notifications/notifications.dart
@@ -1,6 +1,5 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:prism/blocs/jobs/bloc.dart';
 import 'package:prism/blocs/notifications/bloc.dart';
 import 'package:prism/configs/configs.dart';
 import 'package:flutter/material.dart';
--- lib/screens/notifications/widgets/_body.dart
+++ lib/screens/notifications/widgets/_body.dart
@@ -5,7 +5,6 @@ class _Body extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final screenState = _ScreenState.s(context, true);
 
     return SafeArea(
       child: Scaffold(
@@ -18,86 +17,31 @@ class _Body extends StatelessWidget {
           padding: Space.all(),
           child: Column(
             children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: [
-                  Column(
-                    children: [
-                      TextButton(
-                        onPressed: () {
-                          screenState.setIndex(0);
-                        },
-                        child: Text(
-                          'Notifications',
-                          style: AppText.h3bm,
-                        ),
-                      ),
-                      if (screenState.index == 0)
-                        AnimatedContainer(
-                          duration: const Duration(seconds: 1),
-                          height: 2,
-                          width: 100.w,
-                          color: AppTheme.c.primary,
-                        ),
-                    ],
-                  ),
-                  Column(
-                    children: [
-                      TextButton(
-                        onPressed: () {
-                          screenState.setIndex(1);
-                        },
-                        child: Text(
-                          'Applications',
-                          style: AppText.h3bm,
-                        ),
-                      ),
-                      if (screenState.index == 1)
-                        AnimatedContainer(
-                          duration: const Duration(seconds: 1),
-                          height: 2,
-                          width: 100.w,
-                          color: AppTheme.c.primary,
-                        ),
-                    ],
-                  ),
-                ],
-              ),
               Space.y2!,
-              if (screenState.index == 0)
-                BlocBuilder<NotificationsBloc, NotificationsState>(
-                  builder: (context, state) {
-                    if (state.fetch is FetchNotificationLoading ||
-                        state.fetch is FetchNotificationDefault) {
-                      return const Center(child: CircularProgressIndicator());
-                    } else if (state.fetch is FetchNotificationSuccess) {
-                      final notis = state.data ?? [];
-                      return ListView.builder(
-                        itemCount: notis.length,
-                        shrinkWrap: true,
-                        physics: const NeverScrollableScrollPhysics(),
-                        itemBuilder: (context, index) {
-                          final noti = notis[index];
-                          return _NotificationTile(notification: noti);
-                        },
-                      );
-                    } else {
-                      return const ErrorWarning(
-                        title: 'Oops!',
-                        message: 'Could not fetch notifications',
-                      );
-                    }
-                  },
-                )
-              else
-                BlocBuilder<JobsBloc, JobsState>(
-                  builder: (context, state) {
+              BlocBuilder<NotificationsBloc, NotificationsState>(
+                builder: (context, state) {
+                  if (state.fetch is FetchNotificationLoading ||
+                      state.fetch is FetchNotificationDefault) {
+                    return const Center(child: CircularProgressIndicator());
+                  } else if (state.fetch is FetchNotificationSuccess) {
+                    final notis = state.data ?? [];
+                    return ListView.builder(
+                      itemCount: notis.length,
+                      shrinkWrap: true,
+                      physics: const NeverScrollableScrollPhysics(),
+                      itemBuilder: (context, index) {
+                        final noti = notis[index];
+                        return _NotificationTile(notification: noti);
+                      },
+                    );
+                  } else {
                     return const ErrorWarning(
                       title: 'Oops!',
-                      message: 'Could not fetch applications',
+                      message: 'Could not fetch notifications',
                     );
-                  },
-                )
+                  }
+                },
+              )
             ],
           ),
         ),
