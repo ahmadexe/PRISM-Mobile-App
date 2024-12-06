982329c172716d7db0dbcc3b6f1b1d4c0691c703
ahmadexe <muahmad710@gmail.com>
2024-12-07 01:09:43 +0500

    feat: util added to convert image to bytes

---
+++ lib/utils/media/media_utils.dart
@@ -0,0 +1,22 @@
+import 'dart:typed_data';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+class MediaUtils {
+  Future<Uint8List?> getImageBytes(String url) async {
+    try {
+      final response = await http.get(Uri.parse(url));
+
+      if (response.statusCode == 200) {
+        return response.bodyBytes;
+      } else {
+        debugPrint(
+            "Error: Failed to load image with status code ${response.statusCode}");
+        return null;
+      }
+    } catch (e) {
+      debugPrint("Error: $e");
+      return null;
+    }
+  }
+}
