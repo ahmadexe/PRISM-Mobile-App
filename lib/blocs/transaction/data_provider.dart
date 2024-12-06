d5899fdd1c28871922d6519600bf87dbabc5fd77
ahmadexe <muahmad710@gmail.com>
2024-12-06 15:08:03 +0500

    fix: double calls removed

--- lib/blocs/transaction/data_provider.dart
+++ lib/blocs/transaction/data_provider.dart
@@ -1,15 +1,14 @@
 part of 'transaction_bloc.dart';
 
 class _TransactionProvider {
-  static Future<void> createTransaction(Map<String, dynamic> payload, String address) async {
+  static Future<void> createTransaction(
+      Map<String, dynamic> payload, String address) async {
     try {
-
       final client = Dio(BaseOptions(
         contentType: "application/json",
         connectTimeout: const Duration(milliseconds: 60000),
         receiveTimeout: const Duration(milliseconds: 60000),
       ));
-
 
       await client.post(
         'http://$address:11101/transaction',
