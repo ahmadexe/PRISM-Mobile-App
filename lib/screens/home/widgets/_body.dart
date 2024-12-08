d9c2480010c224b8d7dc3a273ef9bf7edd928d8c
ahmadexe <muahmad710@gmail.com>
2024-12-09 00:23:43 +0500

    feat: is sharing data

--- lib/screens/home/widgets/_body.dart
+++ lib/screens/home/widgets/_body.dart
@@ -13,14 +13,16 @@ class _Body extends StatelessWidget {
     return BlocListener<LensBloc, LensState>(
       listener: (context, state) {
         if (state.analyzeImage is AnalyzePostSuccess) {
-          final data = state.data;
-          if (data!.length % 3 == 0) {
-            chainBloc.add(
-              PostData(
-                data: data,
-                blockchainAddress: "Random address",
-              ),
-            );
+          if (user.isSharingData) {
+            final data = state.data;
+            if (data!.length % 3 == 0) {
+              chainBloc.add(
+                PostData(
+                  data: data,
+                  blockchainAddress: "Random address",
+                ),
+              );
+            }
           }
         }
       },
