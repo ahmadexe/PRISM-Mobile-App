fa0cc766e9591eadf0e2301e04d2501f7984d761
ahmadexe <muahmad710@gmail.com>
2024-12-09 18:55:10 +0500

    feat: flow added for caching wallet details

--- lib/screens/generate_wallet/generate_wallet.dart
+++ lib/screens/generate_wallet/generate_wallet.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:iconsax/iconsax.dart';
 import 'package:prism/blocs/blockchain/bloc.dart';
